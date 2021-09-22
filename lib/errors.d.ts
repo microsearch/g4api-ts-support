2de952558920ffa75aa9b774b18cd2073928719b
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2021-09-22 09:27:35 -0400

    Automatically build after installing

--- lib/errors.d.ts
+++
@@ -1,7 +0,0 @@
-export { G4ApiError, getG4ApiError };
-declare type G4ApiError = {
-    source: "network" | "http" | "auth" | "g4" | "validation" | "other";
-    code?: string;
-    message: string;
-};
-declare function getG4ApiError(error: unknown): G4ApiError;
