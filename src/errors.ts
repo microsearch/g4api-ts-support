3461b0678af23cd2d3e3574dc41626156574f3d3
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2021-09-23 07:53:35 -0400

    Use message from Error instances

--- src/errors.ts
+++ src/errors.ts
@@ -9,12 +9,26 @@ type G4ApiError = {
 };
 
 function getG4ApiError(error: unknown): G4ApiError {
-  if (!axios.isAxiosError(error))
-    return { source: "other", message: "unknown error" };
-  if (typeof error.code !== "undefined")
-    return { source: "network", code: error.code, message: error.message };
+  if (!axios.isAxiosError(error)) {
+    return {
+      source: "other",
+      message: error instanceof Error ? error.message : "unknown error",
+    };
+  }
+  if (typeof error.code !== "undefined") {
+    return {
+      source: "network",
+      code: error.code,
+      message: error.message,
+    };
+  }
   const g4error = error.response?.headers["x-g4-error"];
-  if (typeof g4error !== "undefined") return { source: "g4", message: g4error };
+  if (typeof g4error !== "undefined") {
+    return {
+      source: "g4",
+      message: g4error,
+    };
+  }
   const status = error.response?.data.status;
   if (typeof status === "number") {
     switch (status) {
@@ -25,17 +39,25 @@ function getG4ApiError(error: unknown): G4ApiError {
             source: "validation",
             message: JSON.stringify(error.response?.data.errors),
           };
-        } else {
-          return {
-            source: "http",
-            message: statusText,
-          };
         }
+        return {
+          source: "http",
+          message: statusText,
+        };
       case 401:
-        return { source: "auth", message: "unauthorized request" };
+        return {
+          source: "auth",
+          message: "unauthorized request",
+        };
       default:
-        return { source: "http", message: `status: ${status}` };
+        return {
+          source: "http",
+          message: `status: ${status}`,
+        };
     }
   }
-  return { source: "other", message: `${error.name}: ${error.message}` };
+  return {
+    source: "other",
+    message: `${error.name}: ${error.message}`,
+  };
 }
