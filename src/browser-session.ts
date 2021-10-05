d0143beac5797f5b3f047b3bbb3154aabb49d140
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2021-10-05 08:49:47 -0400

    Refactor G4Api class out of G4SessionBrowser

--- src/browser-session.ts
+++ src/browser-session.ts
@@ -1,29 +1,16 @@
 import * as g4 from "g4api-ts";
+import { G4Api, G4ApiOptions } from "./g4api";
 
-export { G4BrowserSession, G4SessionOptions };
+export { G4BrowserSession };
 
 // Since session tokens expire in 15 minutes, setting the refresh interval to
 // just under 5 minutes gives us 3 attempts to refresh the session token before
 // we can no longer talk to the server.
 const REFRESH_INTERVAL = 4.75 * 60 * 1000; // ms
-const REQUEST_TIMEOUT = 30 * 1000; // ms
 
-type G4SessionOptions = {
-  stage: "prod" | "dev" | "test";
-  tenant?: string;
-  application?: string;
-};
-
-class G4BrowserSession {
-  constructor(options: G4SessionOptions) {
-    this.config = {
-      baseURL: `https://g4-${options.stage}.v1.mrcapi.net`,
-      headers: {
-        "x-g4-tenants": options.tenant ?? undefined,
-        "x-g4-application": options.application ?? undefined,
-      },
-      timeout: REQUEST_TIMEOUT,
-    };
+class G4BrowserSession extends G4Api {
+  constructor(options: G4ApiOptions) {
+    super(options);
     this.localStorageKey = `g4-${options.application ?? "app"}-session`;
     try {
       if (this.loadSession()) {
@@ -46,7 +33,7 @@ class G4BrowserSession {
       this.disconnect();
     }
     const response = (await this.auth.authCreate({ username, password })).data;
-    this.applyBearer(response.bearer!);
+    this.bearer = response.bearer;
     this.authentication = response.accessAllowed ? { ...response } : null;
     if (this.connected()) {
       this.interval = setInterval(() => this.syncRefresh(), REFRESH_INTERVAL);
@@ -74,7 +61,7 @@ class G4BrowserSession {
         this.authentication.username = response.username;
         this.authentication.claims = response.claims;
         this.authentication.bearer = response.bearer;
-        this.applyBearer(response.bearer);
+        this.bearer = response.bearer;
         this.saveSession();
       }
     } catch {
@@ -83,107 +70,8 @@ class G4BrowserSession {
     }
   }
 
-  get admin() {
-    return new g4.Admin(this.config);
-  }
-  get admins() {
-    return new g4.Admins(this.config);
-  }
-  get auth() {
-    return new g4.Auth(this.config);
-  }
-  get collections() {
-    return new g4.Collections(this.config);
-  }
-  get document() {
-    return new g4.Document(this.config);
-  }
-  get documents() {
-    return new g4.Documents(this.config);
-  }
-  get exportUsers() {
-    return new g4.ExportUsers(this.config);
-  }
-  get importUsers() {
-    return new g4.ImportUsers(this.config);
-  }
-  get password() {
-    return new g4.Password(this.config);
-  }
-  get policy() {
-    return new g4.Policy(this.config);
-  }
-  get profile() {
-    return new g4.Profile(this.config);
-  }
-  get profileMetadata() {
-    return new g4.ProfileMetadata(this.config);
-  }
-  get profiles() {
-    return new g4.Profiles(this.config);
-  }
-  get role() {
-    return new g4.Role(this.config);
-  }
-  get roleMetadata() {
-    return new g4.RoleMetadata(this.config);
-  }
-  get roles() {
-    return new g4.Roles(this.config);
-  }
-  get session() {
-    return new g4.Session(this.config);
-  }
-  get sync() {
-    return new g4.Sync(this.config);
-  }
-  get tenant() {
-    return new g4.Tenant(this.config);
-  }
-  get tenantMetadata() {
-    return new g4.TenantMetadata(this.config);
-  }
-  get tenants() {
-    return new g4.Tenants(this.config);
-  }
-  get user() {
-    return new g4.User(this.config);
-  }
-  get userClaim() {
-    return new g4.UserClaim(this.config);
-  }
-  get userClaimTokens() {
-    return new g4.UserClaimTokens(this.config);
-  }
-  get userDetails() {
-    return new g4.UserDetails(this.config);
-  }
-  get userEvents() {
-    return new g4.UserEvents(this.config);
-  }
-  get userImport() {
-    return new g4.UserImport(this.config);
-  }
-  get userMetadata() {
-    return new g4.UserMetadata(this.config);
-  }
-  get userPassword() {
-    return new g4.UserPassword(this.config);
-  }
-  get userResetTokens() {
-    return new g4.UserResetTokens(this.config);
-  }
-  get users() {
-    return new g4.Users(this.config);
-  }
-
   private syncRefresh() {
     (async () => await this.refresh())();
-  }
-
-  private applyBearer(bearer: string | null) {
-    this.config.headers["authorization"] =
-      bearer !== null ? `bearer ${bearer}` : undefined;
   }
 
   private loadSession() {
@@ -206,7 +94,6 @@ class G4BrowserSession {
     }
   }
 
-  private config: g4.ApiConfig;
   private localStorageKey: string;
   private interval: number | null = null;
   private authentication: g4.UserAuthenticationResponse | null = null;
---
+++ src/g4api.ts
@@ -0,0 +1,162 @@
+import * as g4 from "g4api-ts";
+
+export { G4ApiOptions, G4Api };
+
+type G4ApiOptions = {
+  stage: "prod" | "dev" | "test";
+  tenant?: string;
+  application?: string;
+};
+
+const DEFAULT_REQUEST_TIMEOUT = 30 * 1000; // ms
+
+class G4Api {
+  constructor(options: G4ApiOptions) {
+    this.config = {
+      baseURL: `https://g4-${options.stage}.v1.mrcapi.net`,
+      headers: {
+        "x-g4-tenants": options.tenant ?? undefined,
+        "x-g4-application": options.application ?? undefined,
+      },
+      timeout: DEFAULT_REQUEST_TIMEOUT,
+    };
+  }
+
+  set bearer(bearer: string) {
+    this.config.headers["authorization"] = `bearer ${bearer}`;
+  }
+
+  set apikey(apikey: string) {
+    this.config.headers["authorization"] = `apikey ${apikey}`;
+  }
+
+  // set timeout in seconds
+  set timeout(timeout: number) {
+    this.config.timeout = timeout * 1000;
+  }
+
+  get admin() {
+    return new g4.Admin(this.config);
+  }
+
+  get admins() {
+    return new g4.Admins(this.config);
+  }
+
+  get auth() {
+    return new g4.Auth(this.config);
+  }
+
+  get collections() {
+    return new g4.Collections(this.config);
+  }
+
+  get document() {
+    return new g4.Document(this.config);
+  }
+
+  get documents() {
+    return new g4.Documents(this.config);
+  }
+
+  get exportUsers() {
+    return new g4.ExportUsers(this.config);
+  }
+
+  get importUsers() {
+    return new g4.ImportUsers(this.config);
+  }
+
+  get password() {
+    return new g4.Password(this.config);
+  }
+
+  get policy() {
+    return new g4.Policy(this.config);
+  }
+
+  get profile() {
+    return new g4.Profile(this.config);
+  }
+
+  get profileMetadata() {
+    return new g4.ProfileMetadata(this.config);
+  }
+
+  get profiles() {
+    return new g4.Profiles(this.config);
+  }
+
+  get role() {
+    return new g4.Role(this.config);
+  }
+
+  get roleMetadata() {
+    return new g4.RoleMetadata(this.config);
+  }
+
+  get roles() {
+    return new g4.Roles(this.config);
+  }
+
+  get session() {
+    return new g4.Session(this.config);
+  }
+
+  get sync() {
+    return new g4.Sync(this.config);
+  }
+
+  get tenant() {
+    return new g4.Tenant(this.config);
+  }
+
+  get tenantMetadata() {
+    return new g4.TenantMetadata(this.config);
+  }
+
+  get tenants() {
+    return new g4.Tenants(this.config);
+  }
+
+  get user() {
+    return new g4.User(this.config);
+  }
+
+  get userClaim() {
+    return new g4.UserClaim(this.config);
+  }
+
+  get userClaimTokens() {
+    return new g4.UserClaimTokens(this.config);
+  }
+
+  get userDetails() {
+    return new g4.UserDetails(this.config);
+  }
+  get userEvents() {
+    return new g4.UserEvents(this.config);
+  }
+
+  get userImport() {
+    return new g4.UserImport(this.config);
+  }
+
+  get userMetadata() {
+    return new g4.UserMetadata(this.config);
+  }
+
+  get userPassword() {
+    return new g4.UserPassword(this.config);
+  }
+
+  get userResetTokens() {
+    return new g4.UserResetTokens(this.config);
+  }
+
+  get users() {
+    return new g4.Users(this.config);
+  }
+
+  private config: g4.ApiConfig;
+}
