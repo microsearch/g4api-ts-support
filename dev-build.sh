46198d08b7aadd5092c50180ba5194725cc5d5f6
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2021-10-27 05:42:49 -0400

    Add dev-build.sh

---
+++ dev-build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# relies on g4api-ts being a sibling of this directory
+pushd ../g4api-ts
+npm run generate-local
+popd
+
+\cp -rv ../g4api-ts/* ./node_modules/g4api-ts
+npm run generate
